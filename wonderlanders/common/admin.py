8f7552131c150df026a7727787d9bde0b4089bd6
elly2chan <elena.iv.konstantinova@gmail.com>
2022-12-10 21:49:34 +0200

    Add Contact model to admin

--- wonderlanders/common/admin.py
+++ wonderlanders/common/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from wonderlanders.common.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'subject')
