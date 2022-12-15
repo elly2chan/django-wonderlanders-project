b5cc642147a3f591604f7feee91b9e8ca4ed81fb
elly2chan <elena.iv.konstantinova@gmail.com>
2022-12-15 13:39:47 +0200

    Remove unused form

--- wonderlanders/store/forms.py
+++ wonderlanders/store/forms.py
@@ -3,13 +3,6 @@ from django import forms
 from wonderlanders.core.form_mixins import AddPlaceholdersFormMixin, RemoveLabelsFormMixin, AddStyleClassFormMixin
 from wonderlanders.store.models import Order
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
-
-
-class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
-
 
 class CheckoutForm(AddPlaceholdersFormMixin, RemoveLabelsFormMixin, AddStyleClassFormMixin, forms.ModelForm):
     class Meta:
